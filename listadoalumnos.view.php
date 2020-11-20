91ade3f3ae052836c3503481e010d6739eb808b7
Investigacion UML Ocotal <cinv.ocotal0201@uml.edu.ni>
2020-11-20 15:08:29 -0600

    Incluido nombre de carrera
    Ahora el listado de alumnos muestra la carrera en la que se matriculó el alumno. Se agregó la tabla carreras con el inner join y se agregó el campo en la sentencia SELECT que trae los datos de la BD. Además se agregó en la tabla del reporte el <td> del nombre de la carrera.

--- listadoalumnos.view.php
+++ listadoalumnos.view.php
@@ -4,7 +4,7 @@ require 'functions.php';
 $permisos = ['Administrador','Profesor'];
 permisos($permisos);
 //consulta los alumnos para el listaddo de alumnos
-$alumnos = $conn->prepare("select a.id, a.num_lista, a.nombres, a.apellidos, a.genero, b.nombre as grado, c.nombre as seccion from alumnos as a inner join grados as b on a.id_grado = b.id inner join secciones as c on a.id_seccion = c.id order by a.apellidos");
+$alumnos = $conn->prepare("select a.id, a.num_lista, a.nombres, a.apellidos, a.genero, carr.nombre, b.nombre as grado, c.nombre as seccion from alumnos as a inner join grados as b on a.id_grado = b.id inner join secciones as c on a.id_seccion = c.id  inner join carreras as carr on carr.idcarrera = a.id_carrera order by a.apellidos");
 $alumnos->execute();
 $alumnos = $alumnos->fetchAll();
 ?>
@@ -45,9 +45,13 @@ $alumnos = $alumnos->fetchAll();
                 </tr>
                 <?php foreach ($alumnos as $alumno) :?>
                 <tr>
-                    <td align="center"><?php echo $alumno['num_lista'] ?></td><td><?php echo $alumno['apellidos'] ?></td>
-                    <td><?php echo $alumno['nombres'] ?></td><td align="center"><?php echo $alumno['genero'] ?></td><td>
-                    <td align="center"><?php echo $alumno['grado'] ?></td><td align="center"><?php echo $alumno['seccion'] ?></td>
+                    <td align="center"><?php echo $alumno['num_lista'] ?></td>
+                    <td><?php echo $alumno['apellidos'] ?></td>
+                    <td><?php echo $alumno['nombres'] ?></td>
+                    <td align="center"><?php echo $alumno['genero'] ?></td>
+                    <td align="center"><?php echo $alumno['nombre'] ?></td>
+                    <td align="center"><?php echo $alumno['grado'] ?></td>
+                    <td align="center"><?php echo $alumno['seccion'] ?></td>
                     <td><a href="alumnoedit.view.php?id=<?php echo $alumno['id'] ?>">Editar</a> </td>
                     <td><a href="alumnodelete.php?id=<?php echo $alumno['id'] ?>">Eliminar</a> </td>
                 </tr>
